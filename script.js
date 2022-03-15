4b56adc21aca3040178128bdc724705b0c77f15f
Shweta Navkar <shwetanavkar18@gmail.com>
2022-03-16 02:20:55 +0530

    Adding more of JS

--- script.js
+++ script.js
@@ -71,3 +71,82 @@ document.querySelector('.nav-links').addEventListener('click', function (e) {
     });
   }
 });
+
+//section 4 Operations
+const tabs = document.querySelectorAll('.operations-tab');
+const tabsContainer = document.querySelector('.operations-tab-container');
+const tabsContent = document.querySelectorAll('.operations-content');
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clicked = e.target.closest('.operations-tab');
+  console.log(clicked);
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations-tab--active'));
+  clicked.classList.add('operations-tab--active');
+
+  tabsContent.forEach(tc => tc.classList.remove('operations-content--active'));
+  document
+    .querySelector(`.operations-content--${clicked.dataset.tab}`)
+    .classList.add('operations-content--active');
+});
+
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav-link')) {
+    const link = e.target;
+
+    const siblings = link.closest('.nav').querySelectorAll('.nav-link');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
+});
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
+});
+
+//sticky navigation
+// const obsCallBack = function () {
+//   d;
+// };
+// const obsOptions = {
+//   root: {},
+// };
+// const observer = new IntersectionObserver(obsCallBack, obsOptions);
+// observer.observe(section1);
+
+// slider
+
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const btn = document.querySelector('.refresh-icon');
+
+let currentSlide = 0;
+const maxSlide = slides.length;
+// slider.style.transform = 'scale(0.9)';
+// slider.style.overflow = 'visible';
+
+console.log(slider);
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+btn.addEventListener('click', function () {
+  if (currentSlide === maxSlide - 1) currentSlide = 0;
+  else currentSlide++;
+  console.log(slider);
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - currentSlide)}%) `)
+  );
+});
+
+// .slide:hover {
+//   filter: blur(8px);
+// }
